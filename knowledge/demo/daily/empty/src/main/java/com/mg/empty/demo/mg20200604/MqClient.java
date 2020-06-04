561f52d7268567ac6b603313ac58db7e205aec3f
matao <mataoshou@163.com>
2020-06-04 18:34:39 +0800

    add mq
    add mq

---
+++ knowledge/demo/daily/empty/src/main/java/com/mg/empty/demo/mg20200604/MqClient.java
@@ -0,0 +1,34 @@
+package com.mg.empty.demo.mg20200604;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class MqClient {
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory factory =new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        factory.setUsername("mg");
+        factory.setPassword("mg");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.basicQos(64);
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("....." + new String(body));
+
+                channel.basicAck(envelope.getDeliveryTag(),false);
+            }
+        };
+
+        channel.basicConsume("mg_queue",consumer);
+
+        Thread.sleep(1000*30);
+
+    }
+}
---
+++ knowledge/demo/daily/empty/src/main/java/com/mg/empty/demo/mg20200604/MqService.java
@@ -0,0 +1,38 @@
+package com.mg.empty.demo.mg20200604;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class MqService {
+
+    public static void main(String[] args) throws Exception {
+//        throw  new Exception(".....");
+        ConnectionFactory factory =new ConnectionFactory();
+        factory.setHost("127.0.0.1");
+        factory.setPort(5672);
+        factory.setUsername("mg");
+        factory.setPassword("mg");
+        Connection connection = factory.newConnection();
+
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare("mg_exchange","direct",true,false,null);
+        channel.queueDeclare("mg_queue",true,false,false,null);
+        channel.queueBind("mg_queue","mg_exchange","mg");
+
+        for(int i=0;i<10;i++) {
+            channel.basicPublish("mg_exchange", "mg", MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    ("Hello"+System.currentTimeMillis()).getBytes());
+            Thread.sleep(1000);
+        }
+
+        Thread.sleep(1000*30);
+
+        channel.close();
+
+    }
+}
