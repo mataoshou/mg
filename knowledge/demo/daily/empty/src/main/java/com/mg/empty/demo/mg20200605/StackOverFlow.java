9daf8c8247c357c8e6d2b059c397bbc972ffe732
mataoshou <mataoshou@163.com>
2020-06-08 00:06:22 +0800

    add jvm
    add jvm

---
+++ knowledge/demo/daily/empty/src/main/java/com/mg/empty/demo/mg20200605/StackOverFlow.java
@@ -0,0 +1,13 @@
+package com.mg.empty.demo.mg20200605;
+
+public class StackOverFlow {
+
+    public static void main(String[] args) {
+        StackOverFlow overFlow = new StackOverFlow();
+        overFlow.each();
+    }
+    public void each()
+    {
+        each();
+    }
+}
