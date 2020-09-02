04d0cc3f27a51b6610ecc4ac24749268dafc665d
mataoshou <mataoshou@163.com>
2020-09-02 23:03:21 +0800

    add security

--- bussiness/compose/compose.auth/src/main/java/com/mg/compose/service/repository/TokenRepository.java
+++ bussiness/compose/compose.auth/src/main/java/com/mg/compose/service/repository/TokenRepository.java
@@ -1,24 +1,24 @@
-package com.mg.compose.service.repository; 
-
-import org.springframework.stereotype.Service;
-import com.mg.compose.pojo.dto.InSysConfigDto;
-import lombok.extern.slf4j.Slf4j;
-import com.mg.compose.pojo.dto.InSysConfigDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.mg.common.pojo.ResultItem;
-import com.mg.compose.service.feign.TokenFeign;
-import java.util.List;
-
-@Service
-@Slf4j
-public class TokenRepository {
-   @Autowired
-   TokenFeign feign;
-
-   
-   public ResultItem login(InSysConfigDto item) throws Exception{
-      ResultItem result =  feign.login(item);
-      return result;
-   }
-
-}
+//package com.mg.compose.service.repository;
+//
+//import org.springframework.stereotype.Service;
+//import com.mg.compose.pojo.dto.InSysConfigDto;
+//import lombok.extern.slf4j.Slf4j;
+//import com.mg.compose.pojo.dto.InSysConfigDto;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import com.mg.common.pojo.ResultItem;
+//import com.mg.compose.service.feign.TokenFeign;
+//import java.util.List;
+//
+//@Service
+//@Slf4j
+//public class TokenRepository {
+//   @Autowired
+//   TokenFeign feign;
+//
+//
+//   public ResultItem login(InSysConfigDto item) throws Exception{
+//      ResultItem result =  feign.login(item);
+//      return result;
+//   }
+//
+//}
--- common/basic/src/main/java/com/mg/common/constant/ResultItemEnum.java
+++ common/basic/src/main/java/com/mg/common/constant/ResultItemEnum.java
@@ -4,7 +4,10 @@ public enum ResultItemEnum {
     RESULT_ENUM_SUCCESS(100,"success"),
     RESULT_ENUM_DENY(-99,"禁止访问"),
     RESULT_ENUM_AUTH(-16,"没有权限"),
-    RESULT_ENUM_FAIL(-1,"fail");
+    RESULT_ENUM_FAIL(-1,"fail"),
+    RESULT_ENUM_EMPTY(15,"empty"),
+    RESULT_ENUM_NULL(16,"null"),
+    RESULT_ENUM_LARGE(105,"large");
 
     private int code;
     private String msg;
